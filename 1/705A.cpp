4d88184ec28ebbd7f253465e1f4e8713fd1e4e4f
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-07 16:53:47 +0300

    Create 705A.cpp

---
+++ 1/705A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) {
+            cout << "I hate";
+        }
+        else if (i % 2 != 0) {
+            cout << " that I hate";
+        } else if (i % 2 == 0) {
+            cout << " that I love";
+        }
+    }
+    cout << " it";
+
+    return 0;
+}
