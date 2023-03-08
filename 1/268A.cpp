2aa5f6c5ed882927fa578b22cadd805eb3791fca
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-08 19:36:05 +0300

    Create 268A.cpp

---
+++ 1/268A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n, h[30], a[30], cnt = 0;
+    
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> h[i] >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j && h[i] == a[j])
+                cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
