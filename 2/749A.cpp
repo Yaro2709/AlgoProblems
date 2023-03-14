46b8534e3bb596b89404a9665320bcb4233d34ad
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-14 20:18:59 +0300

    Create 749A.cpp

---
+++ 2/749A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n / 2 << endl;
+
+    while (n > 3)
+    {
+        cout << 2 << " ";
+        n = n - 2;
+    }
+    cout << n;
+
+    return 0;
+}
