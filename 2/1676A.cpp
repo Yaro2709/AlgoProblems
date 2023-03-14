25e63370970b87692e29ce153fa5586d0a389a60
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-14 18:29:15 +0300

    Create 1676A.cpp

---
+++ 2/1676A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin >> n;
+
+    while (n--) {
+        string s; 
+        cin >> s;
+
+        if (s[0] + s[1] + s[2] == s[3] + s[4] + s[5]) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
