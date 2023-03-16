6c5573c6197e5ac6049c66703b9885229500221b
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-16 23:26:11 +0300

    Create 959A.cpp

---
+++ 2/959A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "Mahmoud";
+    }
+    else {
+        cout << "Ehab";
+    }
+
+    return 0;
+}
