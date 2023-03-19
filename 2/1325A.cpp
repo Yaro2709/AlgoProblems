6acda6def26f25cce5dff617931ae0f086a335b4
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-19 22:46:52 +0300

    Create 1325A.cpp

---
+++ 2/1325A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n; 
+    cin >> n;
+
+    while (n--) {
+        int t; 
+        cin >> t;
+
+        cout << 1 << " " << t - 1 << endl;
+    }
+
+    return 0;
+}
