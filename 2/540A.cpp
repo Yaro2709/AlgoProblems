b3c9174c60f1bf644fdcfb72b4ade554504adcd9
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-21 20:36:00 +0300

    Create 540A.cpp

---
+++ 2/540A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    string a, b;
+    cin >> n >> a >> b;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        ans += min(10 - abs(a[i] - b[i]), abs(a[i] - b[i]));
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
