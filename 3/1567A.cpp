acb8db07bd1c16df7ffa1833e90855a6ed764860
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-23 22:13:50 +0300

    Create 1567A.cpp

---
+++ 3/1567A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t; 
+	cin >> t;
+	while (t--)
+	{
+		int n; 
+		cin >> n;
+
+		string s; 
+		cin >> s;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == 'U') { 
+				cout << 'D'; 
+			}
+			else if (s[i] == 'D') {
+				cout << 'U';
+			}
+			else {
+				cout << s[i];
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
