874caea4551a7e02ea1aeea372557a53016b766a
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-28 19:51:36 +0300

    Create 1455A.cpp

---
+++ 3/1455A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        cout << s.size() <<endl;
+    }
+
+    return 0;
+}
