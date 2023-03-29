5ecdd266d9c12f607ecb4c73d3f1862c2f6dc9e8
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-29 22:42:48 +0300

    Create 1768A.cpp

---
+++ 3/1768A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+
+        cout << a - 1 << endl;
+    }
+
+    return 0;
+}
