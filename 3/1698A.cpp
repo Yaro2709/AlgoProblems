285b1362134a086823f725faacbe3fb195e44e0c
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-03-29 22:36:10 +0300

    Create 1698A.cpp

---
+++ 3/1698A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        cout << v[0] << endl;
+    }
+    return 0;
+}
