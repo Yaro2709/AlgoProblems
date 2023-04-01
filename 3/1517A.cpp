b8bdf8a993838d925588356c312099b5e7df20bc
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-04-01 23:18:54 +0300

    Update 1517A.cpp

--- 3/1517A.cpp
+++ 3/1517A.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-    int t;
+    long long int t;
     cin >> t;
     while (t--) {
-        int n;
+        long long int n;
         cin >> n;
         if (n % 2050 == 0) 
         {
-            int x = n / 2050;
-            int sum = 0;
+            long long int x = n / 2050;
+            long long int sum = 0;
             while (x != 0) 
             {
                 sum += x % 10;
