3ab1968b9f31ca508f7bcc884b0bf3958e86a5d6
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-04-01 23:22:23 +0300

    Create 1304A.cpp

---
+++ 3/1304A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int x, y, a, b;
+        cin >> x >> y >> a >> b;
+        if ((y - x) % (a + b) == 0) {
+            cout << (y - x) / (a + b) << endl;
+        }
+        else {
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
