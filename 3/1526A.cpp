27569e5b3f95c28c822e68030cfbc4328625e439
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-04-09 14:52:33 +0300

    Create 1526A.cpp

---
+++ 3/1526A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long long int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        long long int *arr = new long long int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            cin >> arr[i];
+        }
+
+        sort(arr, arr + 2 * n);
+
+        if (n == 1) {
+            cout << arr[0] << " " << arr[1];
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                cout << arr[n + i] << " " << arr[i] << " ";
+            }
+        }
+        cout << endl;
+
+        delete[] arr;
+    }
+
+    return 0;
+}
