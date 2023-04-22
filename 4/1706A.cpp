80062b809e35ac2302f52818907b635c77c6cf55
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-04-22 21:23:02 +0300

    Create 1706A.cpp

---
+++ 4/1706A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n, m;
+        cin >> n >> m;
+        string s(m, 'B');
+
+        while (n--) 
+        {
+            int x; 
+            cin >> x;
+
+            x--;
+            x = min(x, m - x - 1);
+
+            if (s[x] == 'B') {
+                s[x] = 'A';
+            }
+            else {
+                s[m - x - 1] = 'A';
+            }
+
+        }
+
+        cout << s << endl;
+    }
+
+    return 0;
+}
