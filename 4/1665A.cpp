b4f147aa99f579cd87248fd9001ce1120f9fe79d
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-04-22 22:25:13 +0300

    Create 1665A.cpp

---
+++ 4/1665A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int t; 
+    cin >> t;
+
+    while (t--) 
+    {
+        int n; 
+        cin >> n;
+
+        cout << n - 3 << ' ' << "1 1 1" << endl;
+    }
+
+    return 0;
+}
