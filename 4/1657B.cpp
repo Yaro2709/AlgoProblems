22825cd2d3d4883c828f11ae014589bd4d154c5c
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-04-22 20:49:08 +0300

    Update 1657B.cpp

--- 4/1657B.cpp
+++ 4/1657B.cpp
@@ -8,11 +8,11 @@ int main()
     cin >> t;
     while (t--) 
     {
-        int n, b, x, y;
+        long long int n, b, x, y;
         cin >> n >> b >> x >> y;
 
-        int res = 0; 
-        int ans = 0;
+        long long int res = 0; 
+        long long int ans = 0;
         for (int i = 0; i < n; i++) {
             if (res + x <= b) {
                 res += x;
