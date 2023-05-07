8f0d16a6e85257693d4a0226d1b5eee653f28dc8
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-05-07 20:16:51 +0300

    Create 1393A.cpp

---
+++ 4/1393A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n;
+        cin >> n;
+        cout << (n / 2) + 1 << endl;
+    }
+}
