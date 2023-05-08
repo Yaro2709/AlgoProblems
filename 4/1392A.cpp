f0b78770bac7b5bc080165b613f9411881379903
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-05-08 20:45:02 +0300

    Create 1392A.cpp

---
+++ 4/1392A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector <int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+
+		sort(a.begin(), a.end());
+
+		if (a[0] == a[n - 1])
+			cout << n << endl;
+		else
+			cout << 1 << endl;
+	}
+
+	return 0;
+}
