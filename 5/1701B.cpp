b333b024789fffc641bc2cec1e50ea6bac16d2c9
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-05-12 22:11:29 +0300

    Create 1701B.cpp

---
+++ 5/1701B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << 2 << endl;
+        for (int i = 1; i <= n; ++i) if (i % 2 != 0)
+            for (int j = i; j <= n; j *= 2)
+                cout << j << ' ';
+        cout << endl;
+    }
+
+    return 0;
+}
