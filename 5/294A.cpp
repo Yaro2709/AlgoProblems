4f2761cee3d2190dc5d54206d32f7db8aa4d15b0
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-05-12 21:58:07 +0300

    Create 294A.cpp

---
+++ 5/294A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int* a = new int[n];
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+		if (x < n) {
+			a[x + 1] += a[x] - y;
+		}
+		a[x] = 0;
+		if (x > 1) {
+			a[x - 1] += y - 1;
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		cout << a[i] << endl;
+	}
+
+	delete[] a;
+
+	return 0;
+}
