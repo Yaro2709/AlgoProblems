211f12fe414d768bd668a56461138dfbd945a184
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-05-13 22:05:49 +0300

    Create 832A.cpp

---
+++ 5/832A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n, k;
+    cin >> n >> k;
+    if ((n / k) & 1)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
