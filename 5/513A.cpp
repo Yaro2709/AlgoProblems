0a3bfc3f97be5c8c45d0f7cbc579dc71417afaa1
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-05-17 18:58:05 +0300

    Create 513A.cpp

---
+++ 5/513A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a <= b) cout << "Second";
+    else        cout << "First";
+
+    return 0;
+}
