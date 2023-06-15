cef4b113d95a531602feac08d104dc8fea3924e0
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-06-15 19:58:42 +0300

    Create 1047A.cpp

---
+++ 5/1047A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int n;
+	cin >> n;
+	if (n % 3 == 0) {
+		cout << "1 1 " << (n - 2) << endl;
+	}
+	else {
+		cout << "1 2 " << (n - 3) << endl;
+	}
+
+	return 0;
+}
