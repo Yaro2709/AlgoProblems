b5adc0aaa0583d844ac4c98434d8bd1912e05a50
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-07-07 22:13:50 +0300

    Create 1787A.cpp

---
+++ 5/1787A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 2 == 0)
+            cout << 1 << ' ' << n / 2 << endl;
+        else
+            cout << -1 << endl;
+    }
+
+    return 0;
+}
