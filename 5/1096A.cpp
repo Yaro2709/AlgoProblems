85133eda07ea5b821b85b72b1f48597cf3c8c012
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-07-08 22:35:24 +0300

    Create 1096A.cpp

---
+++ 5/1096A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		cout << a << " " << a * 2 << endl;
+	}
+
+	return 0;
+}
