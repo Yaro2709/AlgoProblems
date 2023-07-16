3c5a1d54ad25285867ae1d5ed7d62fc3505c040b
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-07-16 23:19:13 +0300

    Create 1088A.cpp

---
+++ 6/1088A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << -1 << endl;
+    }
+    else {
+        cout << n << " " << n << endl;
+    }
+    return 0;
+}
