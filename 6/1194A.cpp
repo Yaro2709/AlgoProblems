0fb26f80725ca52ca32bfef395f16bcc33da78fd
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-07-21 22:12:28 +0300

    Create 1194A.cpp

---
+++ 6/1194A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int a[110];
+int main()
+{
+    long long int n, c, d;
+    cin >> n;
+    while (n--)
+    {
+        cin >> c >> d;
+        cout << 2 * d << endl;
+    }
+
+    return 0;
+}
