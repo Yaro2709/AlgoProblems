96b1e276d256e4daff0c8434bf5aca204cb317f5
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-10 22:28:05 +0300

    Create 1104A.cpp

---
+++ 6/1104A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << n << endl;
+    while (--n) cout << "1 ";
+    cout << "1" << endl;
+
+    return 0;
+}
