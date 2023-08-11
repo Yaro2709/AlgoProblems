886b8859a079138477326570fa7f389a9b8d0623
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-11 17:52:28 +0300

    Create 747A.cpp

---
+++ 7/747A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n; 
+	cin >> n;
+	for (int i = sqrt(n); i > 0; i--)
+	{
+		if (n % i == 0)
+		{
+			cout << i << " " << n / i << endl;
+			return 0;
+		}
+	}
+
+	return 0;
+}
