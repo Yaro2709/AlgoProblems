67441fe41c534e6669df78e5ebe7f47eb038b6f2
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-11 13:40:19 +0300

    Update 629A.cpp

--- 7/629A.cpp
+++ 7/629A.cpp
@@ -2,32 +2,21 @@
 
 using namespace std;
 
-int main() {
-    int n, c = 0; 
-    cin >> n;
-    char** arr = new char* [n];
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = new char[n];
-    }
+char c[102][102];
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (arr[i][j] != 'C') continue;
-            for (int a = j + 1; a < n; a++)if (arr[i][a] == 'C')c++;
-            for (int b = i + 1; b < n; b++)if (arr[b][j] == 'C')c++;
-        }
-    }
-    cout << c;
+int main(void) {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)cin >> c[i];
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (c[i][j] == 'C') {
+				for (int q = j + 1; q < n; q++) sum += (c[i][q] == 'C');
+				for (int q = i + 1; q < n; q++) sum += (c[q][j] == 'C');
+			}
 
-    for (int i = 0; i < n; i++)
-    {
-        delete[] arr[i]; 
-    }
-    delete[] arr; 
+	cout << sum << endl;
 
-    return 0;
+	return 0;
 }
