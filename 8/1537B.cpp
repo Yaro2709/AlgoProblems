10ea58d820398a855cca89161946e99a20f08efb
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-12 19:09:35 +0300

    Create 1537B.cpp

---
+++ 8/1537B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long int t, n, m, i, j;
+	cin >> t;
+	while (t--)
+	{
+		cin >> n >> m >> i >> j;
+		i = 1; j = 1;
+		cout << i << " " << j << " " << n << " " << m << endl;
+	}
+
+	return 0;
+}
