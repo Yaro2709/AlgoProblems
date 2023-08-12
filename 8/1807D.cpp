3bfbef5a49f2ce52a6e95835bedf435af03d0df2
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-12 22:34:55 +0300

    Update 1807D.cpp

--- 8/1807D.cpp
+++ 8/1807D.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-const int N = 2e5 + 5;
-
-int a[N];
-int s[N];
-
 void solve()
 {
-	int n, q; cin >> n >> q;
-
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-		s[i] = s[i - 1] + a[i];
-	}
-
-	while (q--)
-	{
-		int l, r, k; cin >> l >> r >> k;
-		int sum = s[n] - (s[r] - s[l - 1]) + (r - l + 1) * k;
-		if (sum % 2 == 1)
-			cout << "YES" << endl;
-		else
-			cout << "NO" << endl;
-	}
+    int n, q; 
+    cin >> n >> q;
+    vector<int>a(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        a[i] += a[i - 1];
+    }
+    while (q--)
+    {
+        int l, r, k;
+        cin >> l >> r >> k;
+        int sum = (a[n] - (a[r] - a[l - 1]) + (r - l + 1) * k);
+        if (sum % 2)cout << "YES\n";
+        else cout << "NO\n";
+    }
 }
 
 int main()
 {
-	int t;
-	cin >> t;
-	while (t--)
-		solve();
+    int t; 
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 
-	return 0;
+    return 0;
 }
