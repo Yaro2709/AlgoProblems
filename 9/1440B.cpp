2d5823d82a743e69ac43ce5956d5b33bbf206060
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-13 19:48:03 +0300

    Update 1440B.cpp

--- 9/1440B.cpp
+++ 9/1440B.cpp
@@ -2,20 +2,20 @@
 
 using namespace std;
 
-#define int long long int
+#define ll long long int
 
 int main() {
-	int t; 
+	ll t;
 	cin >> t;
 	while (t--) {
-		int n, k; 
+		ll n, k;
 		cin >> n >> k;
-		int *a = new int[n * k];
-		for (int i = 0; i < n * k; i++) {
+		ll* a = new ll[n * k];
+		for (ll i = 0; i < n * k; i++) {
 			cin >> a[i];
 		}
-		int ans = 0;
-		for (int i = k; i >= 1; i--) {
+		ll ans = 0;
+		for (ll i = k; i >= 1; i--) {
 			ans += a[n * k - i * ((n + 2) / 2)];
 		}
 		cout << ans << endl;
