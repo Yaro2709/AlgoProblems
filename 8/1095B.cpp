95d6ea9aa7952bc432e24d68ccc75c5818612538
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-13 18:19:05 +0300

    Create 1095B.cpp

---
+++ 8/1095B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int *a = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + n);
+	cout << min(a[n - 1] - a[1], a[n - 2] - a[0]) << endl;
+
+	delete[] a;
+
+	return 0;
+}
