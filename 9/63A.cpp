2b9e63af8616f1d4c1868d9db68b44b0e3d54e95
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-14 15:01:52 +0300

    Create 63A.cpp

---
+++ 9/63A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	string a[100], b[100];
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i] >> b[i];
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (b[i] == "rat")
+			cout << a[i] << endl;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (b[i] == "woman" || b[i] == "child")
+			cout << a[i] << endl;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (b[i] == "man")
+			cout << a[i] << endl;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (b[i] == "captain")
+			cout << a[i] << endl;
+	}
+	return 0;
+}
