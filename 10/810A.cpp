762764c5e1e56cf78bfcf67bfb19d329bcfd0dcc
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-14 16:03:33 +0300

    Create 810A.cpp

---
+++ 10/810A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, k, i, s = 0, a = 0, m[100];
+	double f, g;
+	cin >> n >> k;
+	for (i = 0; i < n; i++) {
+		cin >> m[i];
+		s += m[i];
+	}
+	f = k - 0.5;
+	g = (double)s / n;
+	while (g < f) {
+		s += k;
+		a++;
+		n++;
+		g = (double)(s) / n;
+	}
+	cout << a << endl;
+	return 0;
+}
