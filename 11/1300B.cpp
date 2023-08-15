c4c7672cb779983751d70e021713977800f7998b
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-15 20:06:20 +0300

    Create 1300B.cpp

---
+++ 11/1300B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int *a = new int[2 * n];
+        int s = 0, z = 0;
+ 
+        for (int i = 0; i < 2 * n; i++) {
+            cin >> a[i];
+        }
+ 
+        sort(a, a + 2 * n);
+ 
+        cout << a[n] - a[n - 1] << endl;
+ 
+        delete[] a;
+    }
+ 
+    return 0;
+}
