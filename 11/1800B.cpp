5923b2c567ef07fcde125194a1c7e22b1e041feb
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-15 20:07:40 +0300

    Update 1800B.cpp

--- 11/1800B.cpp
+++ 11/1800B.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int main() {
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, k;
+		cin >> n >> k;
+		string a;
+		int s[200] = { 0 };
+		cin >> a;
+		for (int i = 0; i < a.size(); i++)
+		{
+			s[a[i]]++;
+		}
+		int sum = 0, ans = 0;
+		for (int i = 'a'; i <= 'z'; i++)
+		{
+			sum += min(s[i], s[i - 32]);
+			ans += abs(s[i] - s[i - 32]) / 2;
+		}
+		sum += min(ans, k);
+		cout << sum << endl;
+	}
 
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        int *a = new int[2 * n];
-        int s = 0, z = 0;
-
-        for (int i = 0; i < 2 * n; i++) {
-            cin >> a[i];
-        }
-
-        sort(a, a + 2 * n);
-
-        cout << a[n] - a[n - 1] << endl;
-
-        delete[] a;
-    }
-
-    return 0;
+	return 0;
 }
