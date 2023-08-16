3346dbf370f679cd12be58bf8329bb5c3974b501
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-16 11:55:03 +0300

    Create 1051B.cpp

---
+++ 11/1051B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long int l, r;
+	cin >> l >> r;
+	cout << "YES\n";
+	for (long long int i = l; i <= r; i += 2) {
+		cout << i << ' ' << i + 1 << '\n';
+	}
+
+	return 0;
+}
