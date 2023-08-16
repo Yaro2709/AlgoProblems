03c1681b635a83ac1694384484fc0215b4bcf805
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-16 12:44:09 +0300

    Create 1073A.cpp

---
+++ 12/1073A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	string s;
+	cin >> n >> s;
+	for (int i = 1; i < n; i++) {
+		if (s[i] != s[i - 1]) {
+			cout << "YES" << endl;
+			cout << s[i - 1] << s[i] << endl;
+			return 0;
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
+}
