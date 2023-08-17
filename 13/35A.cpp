66203d05b7e37a23ad0bce291a502a22f1031765
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-17 15:46:54 +0300

    Update 35A.cpp

--- 13/35A.cpp
+++ 13/35A.cpp
@@ -2,18 +2,17 @@
 
 using namespace std;
 
-int main()
-{
-    int s;
-    cin >> s;
-    for (int i = 0; i < 3; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        if (s == a) s = b;
-        else if (s == b) s = a;
-    }
-    cout << s;
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
+    int x; cin >> x;
+    int c[4];
+    c[1] = 1; c[2] = 2; c[3] = 3;
+    for (int i = 0; i < 3; ++i) {
+        int a, b; cin >> a >> b;
+        swap(c[a], c[b]);
+    }
+    cout << c[x];
     return 0;
 }
