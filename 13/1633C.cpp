324e56d6cc4b44e15b157e1f2e8bf587b41b580b
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-18 13:45:22 +0300

    Update 1633C.cpp

--- 13/1633C.cpp
+++ 13/1633C.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main()
 {
-    int t;
+    long long int t;
     cin >> t;
     while (t--) {
-        int hc, dc, hm, dm;
+        long long int hc, dc, hm, dm;
         cin >> hc >> dc >> hm >> dm;
-        int k, w, a, r = 0;
+        long long int k, w, a, r = 0;
         cin >> k >> w >> a;
-        for (int i = 0; i <= k; i++) {
-            int p = (hc + (k - i) * a) / dm;
-            int q = hm / (dc + i * w);
+        for (long long int i = 0; i <= k; i++) {
+            long long int p = (hc + (k - i) * a) / dm;
+            long long int q = hm / (dc + i * w);
             if ((hc + (k - i) * a) % dm != 0) p++;
             if (hm % (dc + i * w) != 0) q++;
             if (p >= q) {
