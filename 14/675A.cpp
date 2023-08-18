e06a597fc59be31dc634828e805c8aed40a7fe09
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-18 16:44:09 +0300

    Create 675A.cpp

---
+++ 14/675A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	long long int a, b, c;
+	cin >> a >> b >> c;
+	if (c == 0)
+	{
+		if (a == b)
+			cout << "YES";
+		else
+			cout << "NO";
+	}
+	else
+	{
+		if ((b - a) % c == 0 && (b - a) / c >= 0)
+			cout << "YES";
+		else
+			cout << "NO";
+	}
+	return 0;
+}
