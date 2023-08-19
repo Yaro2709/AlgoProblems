bc81da64e4da519c7c79b56c07eac67fe285f7fc
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-19 22:08:14 +0300

    Create 1099B.cpp

---
+++ 15/1099B.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+typedef long long ll;
+
+#define ft first
+#define sd second
+
+ll a[10000], b[10000];
+ll ans;
+
+int main()
+{
+	double n;
+	cin >> n;
+	cout << ceil(sqrt(n) * 2);
+	return 0;
+}
