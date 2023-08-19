2179ffb7a091617ba957bd928ed7c7918c41c767
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-19 15:37:22 +0300

    Create 285A.cpp

---
+++ 14/285A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i(k + 1); i; --i)
+        cout << i << ' ';
+    for (int i(k + 2); i <= n; ++i)
+        cout << i << ' ';
+
+    return 0;
+}
