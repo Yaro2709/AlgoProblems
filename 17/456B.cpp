121a5c9ce89a7688a56aa709e67e545a3a727931
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-21 17:56:39 +0300

    Create 456B.cpp

---
+++ 17/456B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int a;
+    cin >> a;
+    if (a % 4 == 0)
+        cout << "4";
+    else
+        cout << "0";
+
+    return 0;
+}
