3a5c3d13d22309ef219e3721e1e08fa4ffd2139b
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-21 15:32:22 +0300

    Create 1455B.cpp

---
+++ 16/1455B.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int t; 
+	cin >> t;
+	while (t--) {
+		int x; cin >> x;
+
+		int steps = 0;
+		while (steps * (steps + 1) < 2 * x)
+			steps++;
+
+		if (steps * (steps + 1) / 2 == x + 1)
+			steps++;
+		cout << steps << endl;
+	}
+
+	return 0;
+}
