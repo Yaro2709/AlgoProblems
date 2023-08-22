b6fad2c3bb79d529ca32660b721ee325e402e36e
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-22 20:19:54 +0300

    Create 1582C.cpp

---
+++ 17/1582C.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	string s;
+	cin >> n >> s;
+	int ans = n + 1;
+	for (int c = 0; c < 26; c++)
+	{
+		int l = 0, r = n - 1, cnt = 0;
+
+		while (l <= r)
+		{
+			if (s[l] == s[r])
+			{
+				l++, r--;
+			}
+
+			else if (s[l] == char('a' + c))
+			{
+				cnt++, l++;
+			}
+			else if (s[r] == char('a' + c))
+			{
+				cnt++, r--;
+			}
+			else
+			{
+				cnt = n + 1;
+				break;
+			}
+		}
+		ans = min(ans, cnt);
+	}
+	if (ans == n + 1) ans = -1;
+	cout << ans << '\n';
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		solve();
+	}
+
+	return 0;
+}
