3ab8babc47deac53cd07ea72cf018e3237a8bbd1
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-22 22:24:48 +0300

    Create 133B.cpp

---
+++ 18/133B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#define ll long long int
+
+ll val[256];
+const ll MOD = 1000003;
+
+int main()
+{
+    val['>'] = 8;
+    val['<'] = 9;
+    val['+'] = 10;
+    val['-'] = 11;
+    val['.'] = 12;
+    val[','] = 13;
+    val['['] = 14;
+    val[']'] = 15;
+    string s;
+    cin >> s;
+    ll ans = 0;
+    for (char c : s) {
+        ans = (ans * 16 + val[c]) % MOD;
+    }
+    cout << ans;
+
+    return 0;
+}
