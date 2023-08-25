f1659521b0fd8e75eeed866896ec461d54f4c4f1
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-25 14:51:54 +0300

    Create 1249B2.cpp

---
+++ 20/1249B2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() 
+{
+	int q;
+	cin >> q;
+	for (int i = 0; i < q; ++i) {
+		int n;
+		cin >> n;
+		vector<int> p(n);
+		for (int j = 0; j < n; ++j) {
+			cin >> p[j];
+			--p[j];
+		}
+		vector<int> used(n);
+		vector<int> ans(n);
+		for (int j = 0; j < n; ++j) {
+			if (!used[j]) {
+				vector<int> cur;
+				while (!used[j]) {
+					cur.push_back(j);
+					used[j] = true;
+					j = p[j];
+				}
+				for (auto el : cur) ans[el] = cur.size();
+			}
+		}
+		for (int j = 0; j < n; ++j) cout << ans[j] << " ";
+		cout << endl;
+	}
+
+	return 0;
+}
