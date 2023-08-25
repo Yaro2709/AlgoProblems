9eb458ba09656805657d41b6f32be51df73a5d6f
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-25 17:08:06 +0300

    Update 1736C1.cpp

--- 20/1736C1.cpp
+++ 20/1736C1.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-signed main() {
-    int t;
+int main() {
+    long long int t;
     cin >> t;
     while (t--) {
-        int n;
+        long long int n;
         cin >> n;
-        int *a = new int[n];
-        int ans = 0;
+        long long int* a = new long long int[n];
+        long long int ans = 0;
         for (int i = 0; i < n; i++)cin >> a[i];
-        int *dp = new int[n + 2];
+        long long int* dp = new long long int[n + 2];
         dp[1] = 0;
-        for (int i = 0; i < n; i++) {
+        for (long long int i = 0; i < n; i++) {
             dp[i + 2] = min(dp[i + 1] + 1, a[i]);
             ans += dp[i + 2];
         }
