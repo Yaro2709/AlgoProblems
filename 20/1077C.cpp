50191b47a646e6525deeadf7d765002d122d9c6b
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-25 16:43:00 +0300

    Update 1077C.cpp

--- 20/1077C.cpp
+++ 20/1077C.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-int a[200010];
-int t[1000010];
+long long int a[200010];
+long long int t[1000010];
 
 int main() {
-    int n;
+    long long int n;
     cin >> n;
-    int ma = 0, sum = 0;
-    for (int i = 1; i <= n; i++) {
+    long long int ma = 0, sum = 0;
+    for (long long int i = 1; i <= n; i++) {
         cin >> a[i];
         t[a[i]]++;
         sum += a[i];
         ma = max(ma, a[i]);
     }
-    int cnt = 0;
-    vector <int> ans;
-    for (int i = 1; i <= n; i++) {
-        int x = sum - a[i];
+    long long int cnt = 0;
+    vector <long long int> ans;
+    for (long long int i = 1; i <= n; i++) {
+        long long int x = sum - a[i];
         if (x % 2) continue;
         x /= 2;
         if (x <= ma && (x != a[i] && t[x]) || (x == a[i] && t[x] - 1)) {
@@ -29,6 +29,6 @@ int main() {
         }
     }
     cout << cnt << endl;
-    for (int i = 0; i < ans.size(); i++) cout << ans[i] << ' ';
+    for (long long int i = 0; i < ans.size(); i++) cout << ans[i] << ' ';
     return 0;
 }
