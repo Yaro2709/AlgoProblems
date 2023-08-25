d27fc57ba1e5d4a823303a5a59b45f50ab42afba
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-25 15:22:42 +0300

    Update 1326C.cpp

--- 20/1326C.cpp
+++ 20/1326C.cpp
@@ -1,26 +1,33 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
-int main() {
-    int n, k, ct, mod = 998244353, ls, xxo;
+#define ll long long
+
+int main()
+{
+    ll n, k; 
     cin >> n >> k;
-    int *p = new int[n + 1];
-    ct = k;
-    ls = -1;
-    int tot = 1, s = 0, ans = 1;
-    for (int i = 1; i <= n; i++)
+    ll *p = new ll[n];
+    ll a = n - k + 1, b = k;
+    vector<ll> v;
+    for (int i = 0; i < n; i++)
     {
         cin >> p[i];
-        if (p[i] <= n && p[i] >= n - k + 1)
-        {
-            s += p[i];
-            if (ls != -1)
-                ans = (ans * (i - ls)) % mod;
-            ls = i;
-        }
+        if (p[i] >= a) v.push_back(i);
     }
-    cout << s << " " << ans;
+    if (k & 1) a = (a + n) / 2;
+    else { b = b / 2; a = a + n; }
+    ll sum = a * b;
+    ll res = 1;
+    ll m = 998244353;
+
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        res = (res * (v[i + 1] - v[i])) % m;
+    }
+    cout << sum << " " << res << endl;
 
     delete[] p;
 
