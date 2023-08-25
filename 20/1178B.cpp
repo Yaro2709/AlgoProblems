c6306fbecf8ad7435d6c83e3f2d7ee7212745ce7
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-25 17:25:20 +0300

    Update 1178B.cpp

--- 20/1178B.cpp
+++ 20/1178B.cpp
@@ -6,8 +6,8 @@ int main()
 {
 	string s;
 	cin >> s;
-	int a = 0, b = 0, c = 0;
-	for (int i = 1; i < s.size(); i++) {
+	long long int a = 0, b = 0, c = 0;
+	for (long long int i = 1; i < s.size(); i++) {
 		if (s[i] == 'o')
 			b += a;
 		else if (s[i - 1] == 'v') {
