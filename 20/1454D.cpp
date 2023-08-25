2c64dc1346ad78a1f75db5fb2869265a9f5dfe73
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-25 14:08:42 +0300

    Create 1454D.cpp

---
+++ 20/1454D.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, f = 0, e = 0, h;
+        cin >> n;
+        ll i;
+        ll k = n;
+        vector<ll> v;
+        for (i = 2; i < sqrt(n) + 1; i++)
+        {
+            f = 0;
+
+            while (k % i == 0)
+            {
+                f++;
+                k /= i;
+            }
+            if (f > e)
+            {
+                e = f;
+                h = i;
+            }
+        }
+        while (n % h == 0)
+        {
+            v.push_back(h);
+            n /= h;
+        }
+        if (v.size() == 0)
+            v.push_back(1);
+
+        cout << v.size() << endl;
+        for (i = 0; i < v.size() - 1; i++)
+            cout << v[i] << " ";
+        cout << v[v.size() - 1] * n << endl;
+    }
+
+    return 0;
+}
